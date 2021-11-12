71144bc48754d6e3942846114202dbf2af295d30
aabhasTonwer <aabhastonwer@gmail.com>
2021-11-11 20:04:14 -0800

    added space

--- create_combined_csv.py
+++ create_combined_csv.py
@@ -2,12 +2,15 @@ import json
 import pandas as pd
 import csv
 
+
 import os
 
 curr_file = os.path.dirname(os.path.abspath(__file__))
 average_delay_file = curr_file + "/data/average_delay_in_release.csv"
 num_email_file = curr_file + "/data/no_of_emails.csv"
 metric_json_file = curr_file + "/repo_metrics.json"
+
+new_data_file = curr_file + "/data/op.json"
 
 print(metric_json_file)
 
@@ -40,19 +43,19 @@ def extract_data_from_csv_delay():
 
 def create_csv_from_metric_json():
 
-    with open('op3.csv','w') as w:
+    with open(curr_file + '/output/op4.csv','w') as w:
         wtr = csv.writer(w)
-        wtr.writerow(["Repo Name","Release Count", "Commit Count", "PR Count", "Average Latency", "Average Comments", "Contributors", "Number of emails", "Average delay in release"])
-        num_of_emails_data = extract_data_from_csv()
-        average_delay_data = extract_data_from_csv_delay()
-        print(num_of_emails_data)
-        print(average_delay_data)
+        wtr.writerow(["Repo Name","Release Count", "Commit Count", "PR Count", "Average Latency", "Average Comments", "Contributors"])
+        # num_of_emails_data = extract_data_from_csv()
+        # average_delay_data = extract_data_from_csv_delay()
+        # print(num_of_emails_data)
+        # print(average_delay_data)
 
-        for k, val in num_of_emails_data.items():
-            if k in average_delay_data:
-                print(k)
+        # for k, val in num_of_emails_data.items():
+        #     if k in average_delay_data:
+        #         print(k)
         # exit()
-        with open(metric_json_file, 'r') as j:
+        with open(new_data_file, 'r') as j:
             data = json.load(j)
             for el in data["data"]:
                 release_count = 0
@@ -68,11 +71,11 @@ def create_csv_from_metric_json():
                 src_name = el["src_repo_name"].split("/")
                 src_name = src_name[-1]
 
-                if src_name in num_of_emails_data and src_name in average_delay_data:
-                    num_of_emails_in_repo = num_of_emails_data[src_name]
-                    average_delay = average_delay_data[src_name]
-                else:
-                    continue
+                # if src_name in num_of_emails_data and src_name in average_delay_data:
+                #     num_of_emails_in_repo = num_of_emails_data[src_name]
+                #     average_delay = average_delay_data[src_name]
+                # else:
+                #     continue
                 for repo in el['repos']:
                     release_count += repo['release_count']
                     commit_count += repo['commit_count']
@@ -85,7 +88,7 @@ def create_csv_from_metric_json():
                 if pr_count != 0:
                     avg_latency = total_latency / pr_count
                     avg_comments = total_comments / pr_count
-                wtr.writerow([src_name, release_count, commit_count, pr_count, avg_latency, avg_comments, contributors , num_of_emails_in_repo, average_delay])
+                wtr.writerow([src_name, release_count, commit_count, pr_count, avg_latency, avg_comments, contributors ])
 
 if "__main__" == __name__:
     create_csv_from_metric_json()
